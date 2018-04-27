39ce8f9bf1ba73a7757361b050510440d4da0162
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-04-27 23:35:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,22 +9,65 @@ function setCart(c) {
   return cart;
 }
 
+//Tests 1-6 -- Passed
 function addToCart(item) {
- // write your code here
+  let price = (Math.floor(Math.random() * 100) + 1);
+  var newItem = {itemName: item, itemPrice: price};
+  cart.push(newItem);
+  return `${item} has been added to your cart.`;
 }
 
+//Tests 7-10 -- Passed
 function viewCart() {
-  // write your code here
+  if(cart.length <= 0) {
+    return "Your shopping cart is empty.";
+  }else if(cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  }else if(cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+  }else {
+    var newArray = [];
+    for(let i = 0; i < cart.length - 1; i++) {
+      newArray.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`);
+    }return `In your cart, you have${newArray}, and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`;
+    }
 }
 
+//Test 11 -- Passed - but see if you can reduce
 function total() {
-  // write your code here
+  var priceArray = [];
+  for(var price=0; price<cart.length; price++) {
+    priceArray.push(cart[price].itemPrice);
+  }
+  var totalPrice = 0;
+  for(var total=0; total<priceArray.length; total++){
+    totalPrice += priceArray[total];
+  }return totalPrice;
 }
+
+//Tests 12 and 13
 
 function removeFromCart(item) {
-  // write your code here
+  if(item in cart === false) {
+    return "That item is not in your cart.";
+  }else {
+    for(var identity=0; identity<=cart.length; identity++) {
+      if(cart[identity].itemName === item) {
+        cart.splice(identity,1);
+      }else {
+        return "I'm not sure what to do next.";
+      }
+    }
+  }
 }
 
+//Tests 14-16
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === undefined) {
+    return "Sorry, we don't have a credit card on file for you.";
+  }else {
+    var yourTotal = total();
+    cart = [];
+    return `Your total cost is $${yourTotal}, which will be charged to the card ${cardNumber}.`;
+  }
 }
